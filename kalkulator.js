371983afd94df262752ca16b4fd3a6fa0416913f
agnespravida <agnespravida27@gmail.com>
2022-03-08 20:28:32 +0700

    initial commit

---
+++ kalkulator.js
@@ -0,0 +1,3 @@
+// Buatlah sebuah class bernama Kalkulator
+// Class tersebut memiliki method untuk tambah, kurang, bagi, kali, akar kuadrat, mencari volume tabung, dan mencari volume kubus
+// Class tersebut dapat digunakan untuk menghitung input berdasarkan method tanpa harus diinstantiate terlebih dahulu
