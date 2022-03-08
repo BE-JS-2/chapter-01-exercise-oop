b9c8168826fddb817b22c3013e975492be430361
agnespravida <agnespravida27@gmail.com>
2022-03-08 20:36:13 +0700

    modify

--- kalkulator.js
+++ kalkulator.js
@@ -1,3 +1,5 @@
 // Buatlah sebuah class bernama Kalkulator
-// Class tersebut memiliki method untuk tambah, kurang, bagi, kali, akar kuadrat, mencari volume tabung, dan mencari volume kubus
+// Class tersebut memiliki method untuk tambah, kurang, bagi, kali, akar kuadrat, mencari luas persegi,mencari volume tabung, dan mencari volume kubus
 // Class tersebut dapat digunakan untuk menghitung input berdasarkan method tanpa harus diinstantiate terlebih dahulu
+
+// tidak perlu menggunakan readline/process.argv untuk memasukkan input, cukup memasukkan input dari kodingan saja
